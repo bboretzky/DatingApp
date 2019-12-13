cfc1bccdaac1d33a57ac9125bd65d3689a1ea4a5
Barry <bboretzky@gmail.com>
2019-12-12 16:35:07 -0800

    End of Section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { AuthService } from "./_services/auth.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
 
+import { AuthService } from "./_services/auth.service";
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
 import { HomeComponent } from "./home/home.component";
@@ -12,7 +13,12 @@ import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
