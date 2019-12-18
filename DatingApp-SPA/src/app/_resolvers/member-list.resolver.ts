90bbd37212f94719a376ab508793563ab5e195c3
Barry <bboretzky@gmail.com>
2019-12-18 15:01:23 -0800

    End of Section 9

---
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -0,0 +1,31 @@
+import { catchError } from "rxjs/operators";
+import { AlertifyService } from "src/app/_services/alertify.service";
+import { UserService } from "src/app/_services/user.service";
+import { Injectable } from "@angular/core";
+import {
+  Resolve,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { User } from "../_models/user";
+import { Observable, of } from "rxjs";
+
+@Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private alertifyService: AlertifyService
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+    return this.userService.getUsers().pipe(
+      catchError(error => {
+        this.alertifyService.error("Problem retrieving data");
+        this.router.navigate(["/home"]);
+        return of(null);
+      })
+    );
+  }
+}
